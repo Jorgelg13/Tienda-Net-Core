511d654fdeb77a078f6cb5f26a99fd56ce8ff83d
Jorge Laj <jorgelg132012@gmail.com>
2020-08-28 22:41:43 -0600

    agregando proyecto de carrito de compras

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/Nuevo.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Modelo;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
+
+namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest { 
+            public DateTime FechaCreacionSesion { get; set; }
+            public List<string> ProductoLista { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly ContextCarrito _context;
+            public Manejador(ContextCarrito context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var carrito = new CarritoSesion
+                {
+                    FechaCreacion = request.FechaCreacionSesion
+                };
+
+                _context.CarritoSesion.Add(carrito);
+                var value = await _context.SaveChangesAsync();
+
+                if (value == 0)
+                {
+                    throw new Exception("Errores en la insercion del carrito");
+                }
+
+                int id = carrito.CarritoSesionId;
+                foreach (var obj in request.ProductoLista)
+                {
+                    var detalleSesion = new CarritoSesionDetalle
+                    {
+                        FechaCreacion = DateTime.Now,
+                        CarritoSesionId = id,
+                        ProductoSeleccionado = obj
+                    };
+
+                    _context.CarritoSesionDetalle.Add(detalleSesion);
+                    value = await _context.SaveChangesAsync();
+                }
+
+                if (value > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el detalle del carrito de compras");
+            }
+        }
+
+    }
+}
--- TiendaServicios.Api.CarritoCompra/Startup.cs
+++ TiendaServicios.Api.CarritoCompra/Startup.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TiendaServicios.Api.CarritoCompra.Aplicacion;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
 
 namespace TiendaServicios.Api.CarritoCompra
 {
@@ -25,6 +29,12 @@ namespace TiendaServicios.Api.CarritoCompra
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ContextCarrito>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("ConexionDatabase"));
+            });
+
+            services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
